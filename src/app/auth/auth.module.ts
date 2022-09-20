4ace98570e98ea4794e1f40102abfa4475894b17
scarleth14 <abigailb643@gmail.com>
2022-09-20 02:30:39 -0600

    Pantalla cambiar contraseña

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,36 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { MainComponent } from './pages/main/main.component';
-import { LoginBoxComponent } from './pages/login/components/login-box/login-box.component';
-import { CarouselComponent } from './pages/login/components/carousel/carousel.component';
-<<<<<<< Updated upstream
-=======
+import { NgModule } from '@angular/core'; 
+import { CommonModule } from '@angular/common'; 
+ 
+import { AuthRoutingModule } from './auth-routing.module'; 
+import { LoginComponent } from './pages/login/login.component'; 
+import { RegistroComponent } from './pages/registro/registro.component'; 
+import { MainComponent } from './pages/main/main.component'; 
+import { LoginBoxComponent } from './pages/login/components/login-box/login-box.component'; 
+import { CarouselComponent } from './pages/login/components/carousel/carousel.component'; 
+import { NgMaterialModule } from '../ng-material/ng-material.module'; 
 import { CambioContrasenaComponent } from './pages/cambio-contrasena/cambio-contrasena.component';
->>>>>>> Stashed changes
-import { NgMaterialModule } from '../ng-material/ng-material.module';
-
-
-@NgModule({
-  declarations: [
-    LoginComponent,
-    RegistroComponent,
-    MainComponent,
-    LoginBoxComponent,
+ 
+ 
+@NgModule({ 
+  declarations: [ 
+    LoginComponent, 
+    RegistroComponent, 
+    MainComponent, 
+    LoginBoxComponent, 
     CarouselComponent,
-    CambioContrasenaComponent,
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-<<<<<<< Updated upstream
-    NgMaterialModule
-=======
-    NgMaterialModule,
->>>>>>> Stashed changes
-  ]
-})
+    CambioContrasenaComponent
+  ], 
+  imports: [ 
+    CommonModule, 
+    AuthRoutingModule, 
+    NgMaterialModule 
+  ] 
+}) 
 export class AuthModule { }
--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -4,14 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-<<<<<<< Updated upstream
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-=======
-import { MatInputModule } from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [],
