efad31109dc72dd6e447742678ec9b25a26486fb
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-10-09 01:07:50 -0600

    Actualizaciones del sidenav con integración del navbar

--- src/app/protegido/shared/sidenav/sidenav.component.ts
+++ src/app/protegido/shared/sidenav/sidenav.component.ts
@@ -1,12 +1,6 @@
-import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
-import { sidenavbarData } from './sinav-data';
-import { SidenavService } from './services/sidenav.service';
-
-/* interface SideNavToggle {
-  screenWidth: number;
-  collapsed: boolean;
-} */
-
+import { Component, OnInit } from '@angular/core';
+import { ItemSideNavData } from './interfaces/sideNavItem.interface';/* INavbarData */
+import { SidenavService } from './services/sidenav.service';/*sidenavbarData === navbarData*/
 
 @Component({
   selector: 'app-sidenav',
@@ -16,36 +10,12 @@ import { SidenavService } from './services/sidenav.service';
 
 export class SidenavComponent implements OnInit {
 
-  /* collapsed: boolean = false; */
+
   sidenavData = this.sidenavService.sidenavbarData;
-
-/*   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
-  collapsed = false;
-  screenWidth = 0;
-  navData = sidenavbarData;
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.screenWidth = window.innerWidth;
-    if(this.screenWidth <= 768 ) {
-      this.collapsed = false;
-      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-    }
-  } */
+  multiple: boolean = false;
 
   ngOnInit(): void {
-      /* this.screenWidth = window.innerWidth; */
   }
-/* 
-  toggleCollapse(): void {
-    this.collapsed = !this.collapsed;
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-  }
-
-  closeSidenav(): void {
-    this.collapsed = false;
-    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
-  } */
 
 
   constructor(private sidenavService:SidenavService) {
@@ -54,6 +24,26 @@ export class SidenavComponent implements OnInit {
 
   }
 
+  handleClick(item: ItemSideNavData): void{
+
+    if(!this.multiple){
+
+      for (let modelItem of this.sidenavData) {
+        
+        if (item !== modelItem && modelItem.expanded) {
+          
+          
+          modelItem.expanded  = false;
+
+        }
+        
+      }
+
+    }
+
+    item.expanded = !item.expanded;
+
+  }
   
 }
 
