dd3c38366b390af28ca0d7ecaf88b3a1d41ef76d
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-10-11 23:28:59 -0600

    Adición Módulo Parámetro

--- src/app/protegido/seguridad/pages/parametro/parametro.component.ts
+++ src/app/protegido/seguridad/pages/parametro/parametro.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-parametro',
   templateUrl: './parametro.component.html',
-  styleUrls: ['./parametro.component.css']
+  styleUrls: ['../usuario/usuario.component.css']
 })
+
 export class ParametroComponent implements OnInit {
 
   constructor() { }
