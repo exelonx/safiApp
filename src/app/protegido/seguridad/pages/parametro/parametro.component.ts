1ef836d22907068c814d9cef87c2e3ff33619f12
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-10-12 00:04:55 -0600

    Tabla de Parametros

--- src/app/protegido/seguridad/pages/parametro/parametro.component.ts
+++ src/app/protegido/seguridad/pages/parametro/parametro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ParametroService } from './services/parametro.service';
 
 @Component({
   selector: 'app-parametro',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class ParametroComponent implements OnInit {
 
-  constructor() { }
+  parametroData = this.parametroService.parametroData; 
+
+  constructor(private parametroService:ParametroService) { }
 
   ngOnInit(): void {
   }
