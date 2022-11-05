4ba7ce2b6a7ce2396b70bb5b1c74319063bf5c2b
Anthony Avila <anthony.avila.aa2@gmail.com>
2022-11-05 03:13:13 -0600

    base de la pantalla de de gestion de producto

--- src/app/protegido/catalogo-ventas/catalogo-ventas.module.ts
+++ src/app/protegido/catalogo-ventas/catalogo-ventas.module.ts
@@ -5,13 +5,19 @@ import { CatalogoVentasRoutingModule } from './catalogo-ventas-routing.module';
 import { GestionProductosComponent } from './pages/gestion-productos/gestion-productos.component';
 import { GestionCategoriaComponent } from './pages/gestion-categoria/gestion-categoria.component';
 
-import { NgMaterialModule } from '../../ng-material/ng-material.module'; 
+import { NgMaterialModule } from '../../ng-material/ng-material.module';
+import { EditarProductoComponent } from './pages/gestion-productos/components/editar/editar-producto.component';
+import { NuevoProductoComponent } from './pages/gestion-productos/components/nuevo/nuevo-producto.component';
+import { ProductoComponent } from './pages/gestion-productos/components/producto/producto.component';
 
 
 @NgModule({
   declarations: [
     GestionProductosComponent,
-    GestionCategoriaComponent
+    GestionCategoriaComponent,
+    ProductoComponent,
+    EditarProductoComponent,
+    NuevoProductoComponent
   ],
   imports: [
     CommonModule,
