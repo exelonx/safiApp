1c5234a8b7fbe94be36f46f473c6e6fae3c3f09a
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-11-04 21:41:25 -0600

    Routing de Pantallas de Catálogo de Ventas

--- src/app/protegido/catalogo-ventas/catalogo-ventas.module.ts
+++ src/app/protegido/catalogo-ventas/catalogo-ventas.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatalogoVentasRoutingModule } from './catalogo-ventas-routing.module';
+import { GestionProductosComponent } from './pages/gestion-productos/gestion-productos.component';
+import { GestionCategoriaComponent } from './pages/gestion-categoria/gestion-categoria.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GestionProductosComponent,
+    GestionCategoriaComponent
+  ],
   imports: [
     CommonModule,
     CatalogoVentasRoutingModule
