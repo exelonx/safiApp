bcc7410d94677c2dcd8d94bcb63d06155d2d1f46
Kevin Cubas <kevin.otaku@hotmail.com>
2022-11-08 09:07:06 -0600

    Corrección en errores de busquedas en tablas, mantenimiento de permisos casi terminado

---
+++ src/app/protegido/seguridad/pages/permiso/pages/permisos-sistema/components/editar-permiso/editar-permiso.component.ts
@@ -0,0 +1,88 @@
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { PermisoService } from '../../../../services/permiso.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../../../../../../../auth/services/auth.service';
+import Swal from 'sweetalert2';
+import { MatCheckbox } from '@angular/material/checkbox';
+
+@Component({
+  selector: 'app-editar-permiso',
+  templateUrl: './editar-permiso.component.html',
+  styleUrls: ['./editar-permiso.component.css']
+})
+export class EditarPermisoComponent implements OnInit {
+
+  // Instancias de elementos HTML
+  @ViewChild('cerrarEditar') cerrarEditar!: MatButton;
+  @ViewChild('consultar') checkConsultar!: MatCheckbox;
+  @ViewChild('editar') checkActualizar!: MatCheckbox;
+  @ViewChild('guardar') checkGuardar!: MatCheckbox;
+  @ViewChild('eliminar') checkEliminar!: MatCheckbox;
+
+  @Output() onActualizacion: EventEmitter<void> = new EventEmitter();
+  
+  constructor(private permisoService: PermisoService, private authService: AuthService) { }
+
+  get permiso() {
+    return this.permisoService.permiso;
+  }
+
+  ngOnInit(): void {
+  }
+
+  enEjecucion: boolean = false;
+
+  // Subscripciones
+  updateSubscripcion!: Subscription;
+
+  actualizar() {
+
+    const id_usuario: number = this.authService.usuario.id_usuario;
+    
+    if( !this.enEjecucion ) {
+    
+      this.enEjecucion = true
+
+      this.permisoService.putPermiso(id_usuario, this.permiso.ID_PERMISO, this.checkGuardar.checked, this.checkEliminar.checked, this.checkActualizar.checked, this.checkConsultar.checked)
+        .subscribe(
+          (resp => {
+            this.onActualizacion.emit();
+            if(resp.ok === true) {
+              this.enEjecucion = false
+              this.cerrarEditar._elementRef.nativeElement.click()
+              Swal.fire({
+                title: '¡Éxito!',
+                text: resp.msg,
+                icon: 'success',
+                iconColor: 'white',
+                background: '#a5dc86',
+                color: 'white',
+                toast: true,
+                position: 'top-right',
+                showConfirmButton: false,
+                timer: 4500,
+                timerProgressBar: true,
+              })
+            } else {
+              this.enEjecucion = false
+              Swal.fire({
+                title: 'Advertencia',
+                text: resp,
+                icon: 'warning',
+                iconColor: 'white',
+                background: '#f8bb86',
+                color: 'white',
+                toast: true,
+                position: 'top-right',
+                showConfirmButton: false,
+                timer: 4500,
+                timerProgressBar: true,
+              })
+            }
+          })
+        )
+    }
+  };
+
+}
--- src/app/protegido/seguridad/pages/permiso/pages/permisos-sistema/permisos-sistema.component.ts
+++ src/app/protegido/seguridad/pages/permiso/pages/permisos-sistema/permisos-sistema.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Rol } from '../../../rol/interfaces/rolItems.interface';
+import { RolService } from '../../../rol/services/rol.service';
+import { AuthService } from '../../../../../../auth/services/auth.service';
+import { Pantalla } from '../../interfaces/pantalla.interfaces';
+import { PageEvent } from '@angular/material/paginator';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { IngresosService } from 'src/app/protegido/services/ingresos.service';
+import { PermisoService } from '../../services/permiso.service';
+import { PermisoSistema } from '../../interfaces/permiso.interfaces';
 
 @Component({
   selector: 'app-permisos-sistema',
@@ -7,12 +17,134 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermisosSistemaComponent implements OnInit {
   
+  @Input() roles: Rol[] = [];
+  @Input() pantallas: Pantalla[] = [];
+
+  @Output() onSeleccionar: EventEmitter<number> = new EventEmitter();
+
+  @ViewChild('selectRol') selectRol!: ElementRef;
+  @ViewChild('selectPantalla') selectPantalla!: ElementRef;
+
+  registros: PermisoSistema[] = [];
+
   // Atributos = controlar paginador y la tabla
-  registros: any[] = [];
-  
-  constructor() { }
+  tamano: number = 0;
+  limite: number = 0;
+  indice: number = -1;
+  desde: string = "0";
+
+  generando: boolean = false;
+
+  // Validador de busqueda
+  buscando: boolean = false;
+
+  // Referencia para páginador
+  paginadorPorReferencia!: PageEvent;
+
+  // Subscripciones
+  subscripcion!: Subscription;
+
+  constructor(private permisoService: PermisoService, private fb: FormBuilder, private usuario: AuthService, private ingresosService: IngresosService) { }
 
   ngOnInit(): void {
+    this.cargarRegistros()
+  }
+
+  deshabilitar(click: any) {
+    click.stopPropagation();
+  }
+
+  // Al entrar por primera vez a la pantalla
+  cargarRegistros() {
+    const id_usuario: number = this.usuario.usuario.id_usuario;
+    
+    this.subscripcion = this.permisoService.getPermisos(id_usuario, '1')
+      .subscribe(
+        resp => {
+          this.registros = resp.permisos!
+          this.tamano = resp.countPermisos!
+          this.limite = resp.limite!
+        }
+      )
+  }
+
+  filtrarPermiso() {
+
+    // Si se ha cambiado el páginador
+    if (this.paginadorPorReferencia) {
+      this.indice = -1;
+    }
+
+    this.desde = "0"
+
+    // Limpiar subscripción
+    this.subscripcion.unsubscribe();
+
+    // data necesaria
+    const id_usuario: number = this.usuario.usuario.id_usuario;
+    const id_rol: string = this.selectRol.nativeElement.value;
+    const id_pantalla: string = this.selectPantalla.nativeElement.value
+    
+    this.subscripcion = this.permisoService.getPermisos(id_usuario, id_rol, id_pantalla)
+      .subscribe(
+        resp => {
+          this.indice = 0;
+          this.registros = resp.permisos!
+          this.tamano = resp.countPermisos!
+          this.limite = resp.limite!
+        }
+      )
+  }
+
+  // Cambiar de página
+  cambioDePagina(evento: PageEvent) {
+
+    // Hacer referencia al páginador
+    this.paginadorPorReferencia = evento
+
+    // Limpiar subscripción
+    this.subscripcion.unsubscribe();
+
+    // Datos requeridos
+    const id_usuario: number = this.usuario.usuario.id_usuario;
+    const id_rol: string = this.selectRol.nativeElement.value;
+    const id_pantalla: string = this.selectPantalla.nativeElement.value
+
+    // Calcular posición de página
+    let desde: string = (evento.pageIndex * evento.pageSize).toString();
+    this.desde = desde;
+
+    console.log(desde)
+    // Consumo
+    this.subscripcion = this.permisoService.getPermisos(id_usuario, id_rol, id_pantalla, "", evento.pageSize.toString(), desde)
+      .subscribe(
+        resp => {
+          this.registros = resp.permisos!
+          this.tamano = resp.countPermisos!
+          this.limite = resp.limite!
+        }
+      )
+  }
+
+  seleccionarPermiso(id: number) {
+    this.onSeleccionar.emit( id )
+  }
+
+  recargar() {
+    // Datos requeridos
+    const id_usuario: number = this.usuario.usuario.id_usuario;
+    const id_rol: string = this.selectRol.nativeElement.value;
+    const id_pantalla: string = this.selectPantalla.nativeElement.value
+
+    // Consumo
+    this.subscripcion = this.permisoService.getPermisos(id_usuario, id_rol, id_pantalla, "", this.limite.toString(), this.desde)
+      .subscribe(
+        resp => {
+          this.registros = resp.permisos!
+          this.tamano = resp.countPermisos!
+          this.limite = resp.limite!
+        }
+      )
   }
 
 }
