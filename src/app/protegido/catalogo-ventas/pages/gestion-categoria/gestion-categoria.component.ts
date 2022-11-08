e502cf59af7174ad30d0ac4ada9024c3c708ec87
Adoniss Ponce <81458410+DoniG09@users.noreply.github.com>
2022-11-08 00:02:07 -0600

    Avances API

--- src/app/protegido/catalogo-ventas/pages/gestion-categoria/gestion-categoria.component.ts
+++ src/app/protegido/catalogo-ventas/pages/gestion-categoria/gestion-categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-gestion-categoria',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionCategoriaComponent implements OnInit {
 
+
+  generando: boolean = false;
+
+  tamano: number = 0;
+  limite: number = 0;
+  indice: number = -1;
+  desde: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Referencia para páginador
+  paginadorPorReferencia!: PageEvent;
+
+  // Cambiar de página
+  cambioDePagina(evento: PageEvent) {
+
+    // Hacer referencia al páginador
+    this.paginadorPorReferencia = evento
+  }
+
+  // Cuando se presione Enter en la casilla buscar
+  buscarRegistro() {
+    // Si se ha cambiado el páginador
+    if (this.paginadorPorReferencia) {
+      this.indice = -1;
+    }
   }
 
 }
