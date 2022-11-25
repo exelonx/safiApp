6e226c0502e197f100bb8fc5985264fe26279b0b
Anthony Avila <anthony.avila.aa2@gmail.com>
2022-11-24 23:40:36 -0600

    editar producto

--- src/app/protegido/catalogo-ventas/pages/gestion-productos/components/editar/editar-producto.component.ts
+++ src/app/protegido/catalogo-ventas/pages/gestion-productos/components/editar/editar-producto.component.ts
@@ -8,8 +8,11 @@ import { PermisosPantallaService } from 'src/app/protegido/services/permisos-pan
 import { ProductoService } from '../../services/producto.service';
 import { Producto } from '../../interfaces/producto.interfaces';
 import Swal from 'sweetalert2';
-import { Insumo } from 'src/app/protegido/inventario/pages/insumo/interfaces/insumo.interface';
+import { Insumo, InsumoResp } from 'src/app/protegido/inventario/pages/insumo/interfaces/insumo.interface';
 import { InsumoService } from 'src/app/protegido/inventario/pages/insumo/services/insumo.service';
+import { ProveedorService } from 'src/app/protegido/inventario/pages/proveedor/services/proveedor.service';
+import { ComprasService } from 'src/app/protegido/inventario/pages/compras/services/compras.service';
+import { Proveedor, ProveedorResp } from 'src/app/protegido/inventario/pages/proveedor/interfaces/proveedorItems.interface';
 
 @Component({
   selector: 'app-editar-producto',
@@ -18,44 +21,118 @@ import { InsumoService } from 'src/app/protegido/inventario/pages/insumo/service
 })
 export class EditarProductoComponent implements OnInit {
 
-  @ViewChild('cerrarCrear') cerrarCrear!: MatButton;
+  @ViewChild('cerrarEditar') cerrarEditar!: MatButton;
+  @ViewChild('totalPagado') totalPagado!: ElementRef;
   @Output() onCerrar: EventEmitter<boolean> = new EventEmitter();
-  @Output() onCrear: EventEmitter<void> = new EventEmitter();
+  @Output() onEditar: EventEmitter<void> = new EventEmitter();
 
   listaInsumo: Insumo[] = [];
+  listaProveedor: Proveedor[] = [];
   
-  manipulado: boolean = false;
   enEjecucion: boolean = false;
+  editandoProveedor: boolean = false;
 
   // Formularios
-  formularioCreacion: FormGroup = this.fb.group({
-    // Formulario
-    nombre:             ['', [Validators.required, Validators.maxLength(100)]],
+  formularioNuevo: FormGroup = this.fb.group({
     // Arreglo de formularios
-    compra: this.fb.array([this.fb.group({
-      insumo: ['', [Validators.required]],
-      cantidad: [1.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$') ]],
-      precio: [0.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$') ]],
-    })], [Validators.required]),
+    compra: this.fb.array([]),
     total: [{value: '0.00', disabled: true}]
   })
 
+  formularioEditar: FormGroup = this.fb.group({
+    // Arreglo de formularios
+    insumo: ["", [Validators.required]],
+    cantidad: [1.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$')]],
+    precio: [0.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$')]]
+  })
+
   get compra() {
-    return this.formularioCreacion.controls['compra'] as FormArray;
+    return this.formularioNuevo.controls['compra'] as FormArray;
   }
 
-  constructor( private authService: AuthService, private fb: FormBuilder, private insumoService: InsumoService,  private productoService: ProductoService ) { }
+  get detalleExistente() {
+    return this.formularioEditar.controls['compraExistentes'] as FormArray;
+  }
 
-  toMayus = InputMayus.toMayusNoReactivo;
-  
+  get compraHecha() {
+    return this.compraService.compra;
+  }
+
+  get detalle() {
+    return this.compraService.detalleCompra;
+  }
+
+  constructor( private authService: AuthService, private fb: FormBuilder, private insumoService: InsumoService, private proveedorService: ProveedorService, private compraService: ComprasService ) { }
+
   async validarNumeros(e: KeyboardEvent) {
     if(e.key === '+' || e.key === '-' || e.key === 'e' || e.key === 'E') {
       e.preventDefault()
     }
   }
 
+  cargarInsumos() {
+    const usuario = this.authService.usuario.id_usuario;
+    this.insumoService.getInsumos(usuario, "", '9999')
+      .subscribe((insumo: InsumoResp) => {
+        this.listaInsumo = insumo.insumos!;
+      });
+  }
+
+  cargarProveedores() {
+    const usuario = this.authService.usuario.id_usuario;
+    this.proveedorService.getProveedores(usuario, "", "9999")
+      .subscribe((proveedor: ProveedorResp) => {
+        this.listaProveedor = proveedor.proveedores!;
+      })
+  }
+
+  agregarInsumo() {
+    this.compra.push(this.fb.group({
+      insumo: ["", [Validators.required]],
+      cantidad: [1.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$')]],
+      precio: [0.00, [Validators.required, Validators.min(0.01), Validators.pattern('^[0-9]+(.[0-9]{0,2})?$')]],
+    }))
+  }
+
+  calcularTotal() {
+    let total: number = 0.00;
+    // Recorrer todo el detalle
+    this.compra.controls.forEach(compra => {
+
+      // Extraer la cantidad y precio de un registro del detalle
+      let cantidad: number = compra.value.cantidad;
+      let precio: number = compra.value.precio;
+
+      // Sumar resultado al total
+      total += cantidad * precio;
+    });
+
+    this.formularioNuevo.get('total')?.setValue(total.toFixed(2))
+  }
+
+  eliminarInsumo( indice: number, click: MouseEvent ) {
+    let total: number = 0.00;
+
+    // Extraer la cantidad y precio del registro por borrar
+    let cantidad: number = this.compra.controls[indice].value.cantidad;
+    let precio: number = this.compra.controls[indice].value.precio;
+
+    // Extraer total
+    total = this.formularioNuevo.controls['total'].value
+
+    // Calcular nuevo total
+    let nuevoTotal: number = total - (cantidad * precio)
+
+    // Establecer nuevo total
+    this.formularioNuevo.get('total')?.setValue(nuevoTotal.toFixed(2))
+
+    // Borrar elemento
+    this.compra.removeAt(indice)
+    click.stopPropagation()
+  }
+
   cerrar() {
-    if(this.manipulado) {
+    if(this.compra.controls.length > 0) {
 
       Swal.fire({
         title: '¡Cambios sin guardar!',
@@ -73,8 +150,7 @@ export class EditarProductoComponent implements OnInit {
       }).then((result) => {
         if (result.isConfirmed) {
           // Cerrar formulario
-          this.manipulado = false;
-          this.cerrarCrear._elementRef.nativeElement.click()
+          this.cerrarEditar._elementRef.nativeElement.click()
   
           // Destruir componente
           setTimeout(() => {
@@ -85,7 +161,7 @@ export class EditarProductoComponent implements OnInit {
 
     } else {
       // Cerrar formulario
-      this.cerrarCrear._elementRef.nativeElement.click()
+      this.cerrarEditar._elementRef.nativeElement.click()
       // Destruir componente
       setTimeout(() => {
         this.onCerrar.emit(false)
@@ -94,58 +170,197 @@ export class EditarProductoComponent implements OnInit {
     
   }
 
-  ngOnInit(): void {
-    /* this.cargarProveedores()
-    this.cargarInsumos() */
+  editar(idDetalle: number, indice: number){
+    let { insumo, precio, cantidad } = this.formularioEditar.value
+
+    this.compraService.putDetalle(idDetalle, insumo, parseFloat(cantidad), parseFloat(precio))
+      .subscribe( resp => {
+        if(resp.ok === true && resp.detalle && resp.compra ) {
+          // Actualizar datos en pantalla
+          this.detalle[indice].CANTIDAD = resp.detalle.CANTIDAD;
+          this.detalle[indice].ID_INSUMO = resp.detalle.ID_INSUMO;
+          this.detalle[indice].PRECIO_COMPRA = resp.detalle.PRECIO_COMPRA;
+          this.compraHecha.TOTAL_PAGADO = resp.compra.TOTAL_PAGADO;
+          this.totalPagado.nativeElement.value = (Number(this.compraHecha.TOTAL_PAGADO).toFixed(2) + 'Lps')
+
+          this.onEditar.emit();
+          Swal.fire({
+            title: '¡Éxito!',
+            text: resp.msg,
+            icon: 'success',
+            iconColor: 'white',
+            background: '#a5dc86',
+            color: 'white',
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
+          })
+          this.detalle[indice].editar = false
+        } else if(resp.ok === true) {
+          Swal.fire({
+            title: 'Sin cambios',
+            text: 'No se realizó ningún cambio',
+            icon: 'info',
+            iconColor: 'white',
+            background: '#3fc3ee',
+            color: 'white',
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+          })
+          this.detalle[indice].editar = false
+        } else {
+          Swal.fire({
+            title: 'Advertencia',
+            text: resp.msg,
+            icon: 'warning',
+            iconColor: 'white',
+            background: '#f8bb86',
+            color: 'white',
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
+          })
+          this.detalle[indice].editar = false
+        }
+      })
   }
 
-  actualizarProducto() {
+  abrirEdicionProveedor(click: MouseEvent) {
+    this.editandoProveedor = true;
+    click.stopPropagation()
+  }
 
+  agregarMasInsumos() {
     if(!this.enEjecucion) {
       this.enEjecucion = true;
+      const total: string = this.formularioNuevo.controls['total'].value
 
-      const id_usuario = this.authService.usuario.id_usuario;      
+      this.compraService.putMasInsumosEnDetalle(this.compra.value, total, this.compraHecha.ID)
+       .subscribe(
+         resp => {
+          if(resp.ok === true && resp.nuevo_detalle){ // Si todo salio bien
+            
+            this.compraHecha.TOTAL_PAGADO = resp.nuevoTotal!;
+            this.totalPagado.nativeElement.value = (Number(this.compraHecha.TOTAL_PAGADO).toFixed(2) + 'Lps')
 
-      /* this.productoService.putProducto()
-        .subscribe(
-          (resp => {
-            this.onActualizacion.emit();
-           
-            if(resp.ok === true) {
-              this.cerrarEditar._elementRef.nativeElement.click();
-              Swal.fire({
-                title: '¡Éxito!',
-                text: resp.msg,
-                icon: 'success',
-                iconColor: 'white',
-                background: '#a5dc86',
-                color: 'white',
-                toast: true,
-                position: 'top-right',
-                showConfirmButton: false,
-                timer: 4500,
-                timerProgressBar: true,
-              })  
-              this.enEjecucion = false;
-            } else {
-              Swal.fire({
-                title: 'Advertencia',
-                text: resp,
-                icon: 'warning',
-                iconColor: 'white',
-                background: '#f8bb86',
-                color: 'white',
-                toast: true,
-                position: 'top-right',
-                showConfirmButton: false,
-                timer: 4500,
-                timerProgressBar: true,
-              })
-              this.enEjecucion = false;
-            }
+            // Insertar nuevo detalle a la factura
+            resp.nuevo_detalle.forEach(insumo => {
+              this.detalle.push(insumo)
+              
+            });
+
+            // Establecer nuevo total
+            this.formularioNuevo.get('total')?.setValue(0.00)
+
+            // Borrar elemento
+            this.compra.clear()
+            this.onEditar.emit();
+            Swal.fire({
+              title: '¡Éxito!',
+              text: resp.msg,
+              icon: 'success',
+              iconColor: 'white',
+              background: '#a5dc86',
+              color: 'white',
+              toast: true,
+              position: 'top-right',
+              showConfirmButton: false,
+              timer: 4500,
+              timerProgressBar: true,
+            })
+
+            this.enEjecucion = false;
+          } else {
+
+            Swal.fire({
+              title: 'Advertencia',
+              text: resp.msg,
+              icon: 'warning',
+              iconColor: 'white',
+              background: '#f8bb86',
+              color: 'white',
+              toast: true,
+              position: 'top-right',
+              showConfirmButton: false,
+              timer: 4500,
+              timerProgressBar: true,
+            })
+            this.enEjecucion = false;
+          }
+
+         }
+       )
+    }
+  }
+
+  eliminar(id_detalle: number, index: number) {
+    this.compraService.deleteItemDetalle(id_detalle)
+      .subscribe( resp => {
+        if(resp.ok === true) {
+          this.compraHecha.TOTAL_PAGADO = resp.nuevoTotal!;
+          this.totalPagado.nativeElement.value = (Number(this.compraHecha.TOTAL_PAGADO).toFixed(2) + 'Lps')
+
+          // Eliminar detalle del formulario
+          this.detalle.splice(index, 1)
+          this.onEditar.emit();
+
+          Swal.fire({
+            title: '¡Éxito!',
+            text: resp.msg,
+            icon: 'success',
+            iconColor: 'white',
+            background: '#a5dc86',
+            color: 'white',
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
           })
-        ) */
-    }  
-  };
+        } else {
+          Swal.fire({
+            title: 'Advertencia',
+            text: resp.msg,
+            icon: 'warning',
+            iconColor: 'white',
+            background: '#f8bb86',
+            color: 'white',
+            toast: true,
+            position: 'top-right',
+            showConfirmButton: false,
+            timer: 4500,
+            timerProgressBar: true,
+          })
+        }
+      })
+  }
+
+  ngOnInit(): void {
+    this.cargarInsumos()
+    this.cargarProveedores()
+  }
+
+  cargarFormulariosEdicion(index: number) {
+
+    this.detalle.forEach((item, i) => {
+      if( i !== index ) {
+        item.editar = false;
+      } else {
+        this.formularioEditar.reset()
+        this.formularioEditar.controls['insumo'].setValue(item.ID_INSUMO)
+        this.formularioEditar.controls['cantidad'].setValue(item.CANTIDAD)
+        this.formularioEditar.controls['precio'].setValue(item.PRECIO_COMPRA)
+      }
+    })
+  }
+
+
 
 }
