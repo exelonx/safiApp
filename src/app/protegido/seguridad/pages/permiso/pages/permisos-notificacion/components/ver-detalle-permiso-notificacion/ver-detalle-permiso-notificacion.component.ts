3136dcbd17c39467f7e8db2515c8f44490d5a7ef
Kevin Cubas <kevin.otaku@hotmail.com>
2022-11-25 08:34:59 -0600

    Permiso de notificaciones terminadas, el root siempre recibira notificaciones, los default no recibiran, eliminar compra y editar proveedor de compra terminado

---
+++ src/app/protegido/seguridad/pages/permiso/pages/permisos-notificacion/components/ver-detalle-permiso-notificacion/ver-detalle-permiso-notificacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-detalle-permiso-notificacion',
+  templateUrl: './ver-detalle-permiso-notificacion.component.html',
+  styleUrls: ['./ver-detalle-permiso-notificacion.component.css']
+})
+export class VerDetallePermisoNotificacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
