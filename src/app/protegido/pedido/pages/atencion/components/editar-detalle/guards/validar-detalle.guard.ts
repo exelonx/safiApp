da5a94a08b67afe6523d735ce925c545fcef6949
Kevin Cubas <kevin.otaku@hotmail.com>
2022-11-29 16:24:31 -0600

    diseño pantalla editar

---
+++ src/app/protegido/pedido/pages/atencion/components/editar-detalle/guards/validar-detalle.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { PedidoService } from '../../../services/pedido.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidarDetalleGuard implements CanActivate {
+
+  constructor( private pedidoService: PedidoService,
+    private router: Router ) {}
+    
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const {id_detalle} = route.params
+      console.log(id_detalle)
+    return this.pedidoService.validarDetalle(id_detalle)
+      .pipe(
+        tap( (valido: boolean | any) => {
+
+          if( valido === false ){
+            this.router.navigateByUrl('/main/pedido/atencion')
+          } else {
+          }
+        })
+      )
+  }
+  
+}
